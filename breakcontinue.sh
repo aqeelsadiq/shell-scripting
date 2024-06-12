bcb3ff3ef3e3d9a2d6084768ccc08e8fa833ed43
Muhammad Aqeel Sadiq <114500109+aqeelsadiq@users.noreply.github.com>
2024-06-12 17:37:24 +0500

    Update breakcontinue.sh

--- breakcontinue.sh
+++ breakcontinue.sh
@@ -3,16 +3,16 @@
 #Break -  it is used to break the execution of loop
 #continuous - it skip the execution on some certain condition
 
-# for((i=0;i<=10;i++))
-# {
-#     if [ $i -eq 6 ]
-#     then
-#     break
-#     else
-#     echo $i
-#     fi
+for((i=0;i<=10;i++))
+{
+    if [ $i -eq 6 ]
+    then
+    break
+    else
+    echo $i
+    fi
 
-# }
+}
 
 #continuos - cont statement skip that particular part that you mention in your condition.
 for((i=0;i<=10;i++))
